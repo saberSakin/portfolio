721549f5edaf7c78486f0a274c8b616a9ef3d485
Mohammad Shakin <md.shakin18@gmail.com>
2024-03-08 21:50:55 +0600

    updates

--- components/Socials.js
+++ components/Socials.js
@@ -7,11 +7,25 @@ import {
   RiInstagramLine,
 } from "react-icons/ri";
 
+import { TbBookDownload } from "react-icons/tb";
+
 import { IoMailOutline } from "react-icons/io5";
 
 const Socials = () => {
   return (
     <div className="flex items-center gap-x-5 text-lg">
+    <div className="flex items-center"> {/* Wrap icon and text in a div */}
+    <Link
+    href={"https://drive.google.com/drive/folders/1GS9nS6_XqZxYiOgrSFf148_aTWywo9Iu?usp=sharing"}
+    target="blank"
+    className="flex items-center hover:text-accent transition-all duration-300"
+    >
+          <span style={{ marginRight: '5px' }}>Download CV</span> {/* Text beside the icon */}
+        <TbBookDownload />
+    </Link>
+  </div>
+
+
       <Link
         href={"https://www.facebook.com/54k1N"}
         target="blank"
